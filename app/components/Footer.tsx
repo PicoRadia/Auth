0fb82988ee699e7ffccfe2dda4c8f579c335faeb
Radia EL HAMDOUNI <53488041+PicoRadia@users.noreply.github.com>
2024-12-09 07:30:23 +0100

    Added a footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer className="py-6 text-center text-sm text-muted-foreground border-t mx-6 md:mx-12 lg:mx-24 xl:mx-32">
+      <p className="mb-1">The Future of Energy is Solar ☀️</p>
+      <p>© Better Earth 2024. All rights reserved.</p>
+    </footer>
+  );
+} 
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,8 @@ import "@/app/globals.css";
 import { ThemeProvider } from "@/app/components/theme-provider";
 import { Navbar } from "@/app/components/Navbar";
 import { SessionProvider } from "next-auth/react";
+import { Footer } from "@/app/components/Footer";
+
 
 import '@/styles/theme.css'
 import '@/styles/layout.css'
@@ -43,10 +45,7 @@ export default function RootLayout({
             <main className="mx-6 mt-20 mb-6 md:mx-12 md:mt-24 lg:mx-24 lg:mt-28 xl:mx-32 flex-1">
               {children}
             </main>
-            <footer className="py-6 text-center text-sm text-muted-foreground border-t mx-6 md:mx-12 lg:mx-24 xl:mx-32">
-              <p className="mb-1">The Future of Energy is Solar ☀️</p>
-              <p>© Better Earth 2024. All rights reserved.</p>
-            </footer>
+            <Footer />
           </ThemeProvider>
         </SessionProvider>
       </body>
